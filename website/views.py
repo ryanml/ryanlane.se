d77b9b715bc2ed3d95d1adf666167fb16a5b0c02
ryanml <rlanese@asu.edu>
2017-02-18 14:03:40 -0700

    Adding core template, creating index view.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,10 @@
+from django.template import loader
 from django.shortcuts import render
+from django.http import HttpResponse as response
 
-# Create your views here.
+def home(request):
+    page_title = "ryanlane.se"
+    template = loader.get_template('website/home.html')
+    context = { 'page_title': page_title }
+    return response(template.render(context, request))
+
